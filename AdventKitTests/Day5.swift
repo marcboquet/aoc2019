384bf68eefcf00187c0a13b53b6fc6305e4021f4
Marc Boquet <marc.boquet@gmail.com>
2019-12-09 07:42:26 +0100

    Too much memory

--- AdventKitTests/Day5.swift
+++ AdventKitTests/Day5.swift
@@ -6,14 +6,14 @@ class Day5: XCTestCase {
     func testExample1() {
         let computer = Computer(program: [3,0,4,0,99])
         let output = try! computer.run(input: 123)
-        XCTAssertEqual(computer.memory, [123,0,4,0,99])
+        XCTAssertEqual(computer.memory[0...4], [123,0,4,0,99])
         XCTAssertEqual(output, [123])
     }
 
     func testExample2() {
         let computer = Computer(program: [1002,4,3,4,33])
         _ = try! computer.run()
-        XCTAssertEqual(computer.memory, [1002,4,3,4,99])
+        XCTAssertEqual(computer.memory[0...4], [1002,4,3,4,99])
     }
 
     func testSolutionPart1() {
