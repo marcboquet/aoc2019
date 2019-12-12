5affe60b6a5ef9735fd833da0d5512a08c74a922
Marc Boquet <marc.boquet@gmail.com>
2019-12-12 10:09:58 +0100

    Day 12 part 2

--- AdventKit/Moons.swift
+++ AdventKit/Moons.swift
@@ -35,6 +35,147 @@ struct Moons {
     var totalEnergy: Int {
         moons.reduce(0, { $0 + $1.energy })
     }
+    
+    func calculateCycleLengthX() -> Int {
+        var moons = Moons(moons: self.moons)
+        let positions = [
+            self.moons[0].position.x,
+            self.moons[1].position.x,
+            self.moons[2].position.x,
+            self.moons[3].position.x
+        ]
+        let velocities = [
+            self.moons[0].velocity.x,
+            self.moons[1].velocity.x,
+            self.moons[2].velocity.x,
+            self.moons[3].velocity.x
+        ]
+        moons.apply(times: 1)
+        var newPositions = [
+        moons.moons[0].position.x,
+        moons.moons[1].position.x,
+        moons.moons[2].position.x,
+        moons.moons[3].position.x
+        ]
+        var newVelocities = [
+        moons.moons[0].velocity.x,
+        moons.moons[1].velocity.x,
+        moons.moons[2].velocity.x,
+        moons.moons[3].velocity.x
+        ]
+        var i = 1
+        while (positions != newPositions || velocities != newVelocities) {
+            moons.apply(times: 1)
+            newPositions = [
+            moons.moons[0].position.x,
+            moons.moons[1].position.x,
+            moons.moons[2].position.x,
+            moons.moons[3].position.x
+            ]
+            newVelocities = [
+            moons.moons[0].velocity.x,
+            moons.moons[1].velocity.x,
+            moons.moons[2].velocity.x,
+            moons.moons[3].velocity.x
+            ]
+            i+=1
+        }
+        return i
+    }
+    
+    func calculateCycleLengthY() -> Int {
+        var moons = Moons(moons: self.moons)
+        let positions = [
+            self.moons[0].position.y,
+            self.moons[1].position.y,
+            self.moons[2].position.y,
+            self.moons[3].position.y
+        ]
+        let velocities = [
+            self.moons[0].velocity.y,
+            self.moons[1].velocity.y,
+            self.moons[2].velocity.y,
+            self.moons[3].velocity.y
+        ]
+        moons.apply(times: 1)
+        var newPositions = [
+        moons.moons[0].position.y,
+        moons.moons[1].position.y,
+        moons.moons[2].position.y,
+        moons.moons[3].position.y
+        ]
+        var newVelocities = [
+        moons.moons[0].velocity.y,
+        moons.moons[1].velocity.y,
+        moons.moons[2].velocity.y,
+        moons.moons[3].velocity.y
+        ]
+        var i = 1
+        while (positions != newPositions || velocities != newVelocities) {
+            moons.apply(times: 1)
+            newPositions = [
+            moons.moons[0].position.y,
+            moons.moons[1].position.y,
+            moons.moons[2].position.y,
+            moons.moons[3].position.y
+            ]
+            newVelocities = [
+            moons.moons[0].velocity.y,
+            moons.moons[1].velocity.y,
+            moons.moons[2].velocity.y,
+            moons.moons[3].velocity.y
+            ]
+            i+=1
+        }
+        return i
+    }
+    
+    func calculateCycleLengthZ() -> Int {
+        var moons = Moons(moons: self.moons)
+        let positions = [
+            self.moons[0].position.z,
+            self.moons[1].position.z,
+            self.moons[2].position.z,
+            self.moons[3].position.z
+        ]
+        let velocities = [
+            self.moons[0].velocity.z,
+            self.moons[1].velocity.z,
+            self.moons[2].velocity.z,
+            self.moons[3].velocity.z
+        ]
+        moons.apply(times: 1)
+        var newPositions = [
+        moons.moons[0].position.z,
+        moons.moons[1].position.z,
+        moons.moons[2].position.z,
+        moons.moons[3].position.z
+        ]
+        var newVelocities = [
+        moons.moons[0].velocity.z,
+        moons.moons[1].velocity.z,
+        moons.moons[2].velocity.z,
+        moons.moons[3].velocity.z
+        ]
+        var i = 1
+        while (positions != newPositions || velocities != newVelocities) {
+            moons.apply(times: 1)
+            newPositions = [
+            moons.moons[0].position.z,
+            moons.moons[1].position.z,
+            moons.moons[2].position.z,
+            moons.moons[3].position.z
+            ]
+            newVelocities = [
+            moons.moons[0].velocity.z,
+            moons.moons[1].velocity.z,
+            moons.moons[2].velocity.z,
+            moons.moons[3].velocity.z
+            ]
+            i+=1
+        }
+        return i
+    }
 }
 
 class Moon {
--- AdventKitTests/Day12.swift
+++ AdventKitTests/Day12.swift
@@ -25,4 +25,42 @@ class Day12: XCTestCase {
         XCTAssertEqual(moons.totalEnergy, 12466)
     }
 
+    func testExample2() {
+        var moons = Moons(moons: [
+        Moon(position: Vector3(x:-1, y:0, z:2)),
+        Moon(position: Vector3(x:2, y:-10, z:-7)),
+        Moon(position: Vector3(x:4, y:-8, z:8)),
+        Moon(position: Vector3(x:3, y:5, z:-1))
+        ])
+        moons.apply(times: 100)
+        XCTAssertEqual(moons.calculateCycleLengthX(), 18)
+        XCTAssertEqual(moons.calculateCycleLengthY(), 28)
+        XCTAssertEqual(moons.calculateCycleLengthZ(), 44)
+        XCTAssertEqual(lcm(a: 18, b: lcm(a: 28, b: 44)), 2772)
+    }
+
+    func testSolutionPart2() {
+        var moons = Moons(moons: [
+        Moon(position: Vector3(x:-4, y:-14, z:8)),
+        Moon(position: Vector3(x:1, y:-8, z:10)),
+        Moon(position: Vector3(x:-15, y:2, z:1)),
+        Moon(position: Vector3(x:-17, y:-17, z:16))
+        ])
+        moons.apply(times: 100)
+        XCTAssertEqual(moons.calculateCycleLengthX(), 186028)
+        XCTAssertEqual(moons.calculateCycleLengthY(), 286332)
+        XCTAssertEqual(moons.calculateCycleLengthZ(), 108344)
+        XCTAssertEqual(lcm(a: 186028, b: lcm(a: 286332, b: 108344)), 360689156787864)
+    }
+
+}
+
+
+private func gcd(a: Int, b: Int) -> Int {
+ 
+    return b == 0 ? a : gcd(a: b, b: a % b)
+}
+
+func lcm(a:Int, b:Int) -> Int {
+    return abs(a * b) / gcd(a: a, b: b)
 }
